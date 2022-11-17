16f43a9e382ee125b74e2978a7aa59687be2b3ce
atillaerturkk <atilla.erturk@etiya.com>
2022-11-17 11:43:34 +0300

    first

---
+++ ödev1.py
@@ -0,0 +1,16 @@
+baslik = "İYİ GÜNLER"
+vade = 23
+faizOrani = 3.4
+print(type(baslik))
+print(type(vade))
+print(type(faizOrani))
+
+mesaj = "Hoşgeldin"
+kullaniciAdi = "Atilla"
+kullaniciSoyadi = "Ertürk"
+print(mesaj+" "+kullaniciAdi+" "+kullaniciSoyadi+"!")
+
+
+sayi1 = 20
+sayi2 = 30
+print(sayi1+sayi2)
